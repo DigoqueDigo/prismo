b246a2611e4816add9b24f71a3c4d046e6cf0a24
DigoqueDigo <diogoacmarques2003@gmail.com>
2025-07-17 18:21:25 +0100

    Add meson build

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <mymath.h>
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!!\n");
+    printf("Sum: %d\n", sum(5, 3));
+    printf("Subtract: %d\n", subtract(5, 3));
+    printf("Multiply: %d\n", multiply(5, 3));
+    printf("Divide: %d\n", divide(5, 3));
+    printf("Fim do programa!\n");
+    return 0;
+}
